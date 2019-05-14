069f1b441660ac59a45ca9557fe9f2bb524de925
MaBrur <mabruraas@gmail.com>
2019-05-14 15:52:46 +0200

    Added consumer and producer for RabbitMQ communication

---
+++ rabbitmq/producer.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+print('Established connection')
+
+# Create a queue
+channel.queue_declare(queue='content')
+print('Queue created')
+
+channel.basic_publish(exchange='',
+        routing_key='content',
+        body=json.dumps({
+            'type': 'face/encoding',
+            's3': 'mybucket/28c98ee8b211be21d4a9f4ef1687c4d36f',
+            'redis': 'redis-ref',
+            })
+        )
+
+print('Published message')
+
+connection.close()
+print('Connection closed')
+
