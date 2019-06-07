67c2943d43e244b743078e551863934b4410e660
MaBrur <mabruraas@gmail.com>
2019-06-07 16:59:34 +0200

    Added thumbnail logic for image_plugin

--- sqapi/plugins/image_plugin/__init__.py
+++ sqapi/plugins/image_plugin/__init__.py
@@ -1,18 +1,39 @@
 import os
+import pathlib
 from datetime import datetime
+
+from PIL import Image
 
 SQL_SCRIPT_DIR = '{}/scripts'.format(os.path.dirname(__file__))
 INSERT_ITEM = 'insert_item.sql'
 
 
 def execute(config, database, message: dict, metadata: dict, data: bytes):
-    print('Metadata: {}'.format(metadata))
-    print('Data processing started')
     content_size = data.seek(0, 2)
-    data.close()
-    print('File size: {}'.format(content_size))
-    print('Data processing ended')
+    data.seek(0, 0)
 
+    create_thumbnail(config, message, data)
+
+    save_to_db(content_size, database, message, metadata)
+
+
+def create_thumbnail(config, message, data):
+    width = config.custom.get('thumb_size', {}).get('width', 128)
+    height = config.custom.get('thumb_size', {}).get('height', 128)
+    out = config.custom.get('thumb_dir')
+
+    pathlib.Path(out).mkdir(parents=True, exist_ok=True)
+
+    try:
+        im = Image.open(data)
+        im.thumbnail((width, height))
+        im.save('{}'.format(os.sep).join([out, message.get('uuid_ref')]), im.format)
+    except IOError as e:
+        err = 'Cannot create thumbnail for {}: {}'.format(data, str(e))
+        raise Exception(err)
+
+
+def save_to_db(content_size, database, message, metadata):
     # This defines the kwargs that are sent in as parameters to the SQL script
     output = {
         'uuid_ref': message.get('uuid_ref', None),
--- sqapi/plugins/image_plugin/blueprints/thumbnails.py
+++ sqapi/plugins/image_plugin/blueprints/thumbnails.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python3
 
-from flask import Blueprint
+from flask import Blueprint, current_app, send_from_directory
 from flask_cors import cross_origin
-
-from api import responding as response
 
 bp = Blueprint(__name__, __name__, url_prefix='/thumbnails')
 
 
-@bp.route('/', methods=['GET'])
+@bp.route('/<img_id>', methods=['GET'])
 @cross_origin()
-def index():
-    return response.ok('[Thumbnail-1, Thumbnail-2, Thumbnail-3]')
+def index(img_id):
+    thumb_dir = get_config('thumb_dir')
+
+    return send_from_directory(directory=thumb_dir, filename=img_id)
+
+
+def get_config(key):
+    return current_app.config.custom.get('image_plugin', {}).get(key, {})
