0c54c9dba5107da81e2ff08551c5cd9f7710b156
MaBrur <mabruraas@gmail.com>
2019-07-24 15:15:33 +0200

    Removed commed from Message object

--- sqapi/core/message.py
+++ sqapi/core/message.py
@@ -1,10 +1,4 @@
 class Message:
-    # TODO:
-    # Should wrapper object for Message be used?
-    # pros/cons:
-    # - Extra fields easier to use (eg. timestamps etc.)
-    # - Standardized transportation within sqAPI
-    # - Is it necessary, or is it only container for message body?
 
     def __init__(self, body: dict):
         self.body = body
