e4d1c10f76cc9bc201a63ef5212cd30b6b0bf851
MaBrur <mabruraas@gmail.com>
2019-08-11 15:05:41 +0200

    Added ulimits for es in compose, and added es searching and indexing for es connector

--- sqapi/connectors/db/elasticsearch.py
+++ sqapi/connectors/db/elasticsearch.py
@@ -1,45 +1,36 @@
 #! /usr/bin/env python3
-import logging
+import json
 
 from elasticsearch import Elasticsearch
 
-from sqapi.core.message import Message
-
-log = logging.getLogger(__name__)
-
 
 class Database:
-
     def __init__(self, config: dict):
         self.cfg = config
-        self.cfg_con = config.get('connection')
+        self.cfg_con_list = config.get('connection') or []
 
     def get_connection(self):
         try:
             cluster = [{
                 'host': c.get('host', 'localhost'),
                 'port': c.get('port', '9200')
-            } for c in self.cfg_con.get('connection')]
-            log.debug('Establishing connection towards: {}'.format(cluster))
-
+            } for c in self.cfg_con_list or []]
+            #            log.debug('Establishing connection towards: {}'.format(cluster))
             es = Elasticsearch(cluster)
-
             return es
-
         except ConnectionError as e:
             err = 'Failed to establish connection to the Elasticsearch cluster, please verify the config: {}'
             format(str(e))
-            log.debug(err)
+            #            log.debug(err)
             raise ConnectionError(err)
 
-    def initialize_database(self):
-        pass
+    def create_document(self, index: str, type: str, body):
+        # Creates a new document(body) of a specified type, with a specific index
+        con = self.get_connection()
+        con.index(index, body, type)
 
-    def execute_script(self, script_path: str, **kwargs):
-        pass
-
-    def execute_query(self, query: str, **kwargs):
-        pass
-
-    def update_message(self, message: Message, status: str, info: str = None):
-        pass
+    def fetch_document(self, index: str, body, query_clause='match'):
+        # Uses Elasticsearch Query DSL:
+        # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl.html
+        con = self.get_connection()
+        con.search(index, body=json.dumps({'query': {query_clause: body}}))
