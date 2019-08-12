ed9c77028373cace098100c0d95f08f3632ab2a8
MaBrur <mabruraas@gmail.com>
2019-08-12 16:32:45 +0200

    Modified ES Index and Search methods for ES Connector. Rewritten blueprint detection - WIP, not working

--- sqapi/connectors/db/elasticsearch.py
+++ sqapi/connectors/db/elasticsearch.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 import json
+import logging
 
 from elasticsearch import Elasticsearch
+
+log = logging.getLogger(__name__)
 
 
 class Database:
@@ -15,22 +18,28 @@ class Database:
                 'host': c.get('host', 'localhost'),
                 'port': c.get('port', '9200')
             } for c in self.cfg_con_list or []]
-            #            log.debug('Establishing connection towards: {}'.format(cluster))
+            log.debug('Establishing connection towards: {}'.format(cluster))
             es = Elasticsearch(cluster)
             return es
         except ConnectionError as e:
             err = 'Failed to establish connection to the Elasticsearch cluster, please verify the config: {}'
             format(str(e))
-            #            log.debug(err)
+            log.debug(err)
             raise ConnectionError(err)
 
-    def create_document(self, index: str, type: str, body):
-        # Creates a new document(body) of a specified type, with a specific index
+    def create_document(self, area: str, body, kind: str = '_doc'):
+        # Creates a new document(body) of a specified type(optional),
+        # within a specific area (index/collection/etc)
         con = self.get_connection()
-        con.index(index, body, type)
+        con.index(area, body, kind)
 
-    def fetch_document(self, index: str, body, query_clause='match'):
+    def fetch_document(self, area: str, body, query_clause='match'):
         # Uses Elasticsearch Query DSL:
         # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl.html
         con = self.get_connection()
-        con.search(index, body=json.dumps({'query': {query_clause: body}}))
+        res = con.search(area, body=json.dumps({'query': {query_clause: body}}))
+
+        return res.get('hits', {}).get('hits', [])
+
+    def initialize_database(self):
+        pass
