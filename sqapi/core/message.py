b733bb1c3f51bf87b95e5d7c222cd86d9094cbe8
MaBrur <mabruraas@gmail.com>
2019-11-23 14:32:48 +0100

    Added hash digest calculation for SHA-256, and mime detection if missing from message. Included filetype dependency

--- sqapi/core/message.py
+++ sqapi/core/message.py
@@ -8,6 +8,8 @@ class Message:
 
         self.msg_fields = config.get('message_fields', {})
 
+        self.hash_digest = None
+
         self.id = str(uuid.uuid4())
         self.uuid = body.get(self._field_key('uuid'))
         self.type = body.get(self._field_key('type'))
@@ -19,3 +21,15 @@ class Message:
         field = self.msg_fields.get(field_name) or {}
 
         return field.get('key') or field_name
+
+    def __str__(self):
+        msg = {
+            'id': self.id,
+            'uuid': self.uuid,
+            'type': self.type,
+            'metadata': self.metadata,
+            'data_location': self.data_location,
+            'meta_location': self.meta_location,
+            'hash_digest': self.hash_digest,
+        }
+        return str(msg)
